1bf796ab70d51a32376e76e60b6564446c8c40e2
Susan Hooper <hoopersusane@gmail.com>
2016-06-30 18:22:52 -0400

    update changes to js and html files

--- server.js
+++ server.js
@@ -1,58 +1,85 @@
+// Dependencies
+// =============================================================
 var inquirer = require('inquirer');
+var express = require('express');
 var bodyParser = require('body-parser');
-var path = require('path')
+var path = require('path');
 
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 3000;
 
+// Sets up the Express app to handle data parsing 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({type:'application/vnd.api+json'}));
 
+// (DATA)
+// =============================================================
+var friends = require(path.join(__dirname, 'app', 'data', 'friends.js'))
 
-var waitForDisplay = true
-var displayProducts = function() {
-        for (var i = 0; i < allprods.length; i++) {
-            console.log("ID: " + allprods[i].ItemID + " || Product Name: " + allprods[i].ProductName + " || Price: " + allprods[i].Price.toFixed(2));
-        }    
-        waitForDisplay = false;
-        userPrompt();
-};
+// Routes
+// =============================================================
 
-     displayProducts();    
+// Basic route that sends the user first to the AJAX Page
 
-var userPrompt = function() {
-        validate: function(value) {
-            if (isNaN(value) == false) {
-                return true;
-            } else {
-                return false;
-            }
-        },
-        filter: Number
+app.get('/', function(req, res){
+  //res.send("survey page")
+  res.sendFile(path.join(__dirname, 'app', 'public', 'home.html'));
+})
 
-        validate: function(value) {
-            if (isNaN(value) == false) {
-                return true;
-            } else {
-                return false;
-            }
-        },
-        filter: Number
+app.get('/home', function(req, res){
+  //res.send("home page")
+  res.sendFile(path.join(__dirname, 'app', 'public', 'home.html'));
+})
 
-           var newValue = ''
-           var oldValue = answer.Quantity
-           newValue = res[0].StockQuantity-answer.Quantity;
-           //console.log(newValue);
-           if (answer.Quantity > res[0].StockQuantity) {
-                console.log("Insufficent Quantity");
-                userPrompt();
-            } else {
-                console.log("Total Price: $" + (Number(answer.Quantity) * Number(res[0].Price))+".00");
-                
-               //connection.query('UPDATE Products SET StockQuantity = :StockQuantity',{StockQuantity: newValue}, ' WHERE ItemID = :ItemID', {ItemID: answer.ItemID}, function(err,res) {
-               connection.query('UPDATE Products SET StockQuantity = ? WHERE ItemID = ?', [newValue, answer.ItemID], function(err,res) {
-               });
-            }
-}
+app.get('/survey', function(req, res){
+  //res.send("Welcome to the Star Wars Page!")
+  res.sendFile(path.join(__dirname, 'app', 'public', 'survey.html'));
+})
 
+// Search for Matches - provides JSON
+app.get('/api/friends', function(req, res){
 
+	res.json(friends);
+  // var chosen = req.params.friends;
 
-// $(document).ready(function() {
-//     $('input#input_text, textarea#textarea1').characterCounter();
-//   });
+  // if(chosen){
+  //   console.log(chosen);
+
+  //   for (var i=0; i <friends.length; i++){
+
+  //     if (chosen == friends[i].routeName){
+  //       res.json(friends[i]);
+  //       return;
+  //     }
+  //   }
+
+  //   res.json(false);
+  // }
+
+  // else{
+  //   res.json(friends);
+  // }
+})
+
+// Create New Characters - takes in JSON input
+app.post('/api/new', function(req, res){
+
+  var newcharacter = req.body;
+  newcharacter.routeName = newcharacter.name.replace(/\s+/g, '').toLowerCase()
+
+  console.log(newcharacter);
+
+  characters.push(newcharacter);
+
+  res.json(newcharacter);
+})
+
+// Starts the server to begin listening 
+// =============================================================
+app.listen(PORT, function(){
+  console.log('App listening on PORT ' + PORT);
+})
